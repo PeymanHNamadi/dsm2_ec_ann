d6ef084688278e36a414d2428dea0c2d3f60886b
PeymanHNamadi <99760532+PeymanHNamadi@users.noreply.github.com>
2025-09-05 14:26:23 -0700

    Add files via upload

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 # assume that the container already has activated the right environment
-panel serve dashboard.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
+panel serve dashboard_v202509.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
